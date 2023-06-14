fd78c91bb5e745dd46ecf9c979d6fdf3512a7312
VannyTep <89964001+VannyTep@users.noreply.github.com>
2023-06-14 15:30:33 +0700

    Completed Level 1 Desgin but not yet script it

---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartScript : MonoBehaviour
+{
+    public GameObject RestartCanvas;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        RestartCanvas.SetActive(true);
+    }
+}
